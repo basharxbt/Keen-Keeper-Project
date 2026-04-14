e7050528aacacc20d5df156830a64ebbc6585219
Shafiullah Bashar <shafiullahbashaar@gmail.com>
2026-04-14 15:09:44 +0600

    Navbar Design  Completed

---
+++ src/app/timeline/page.jsx
@@ -0,0 +1,9 @@
+const Timeline = () => {
+  return (
+    <div>
+      <h1>This Is Timeline</h1>
+    </div>
+  );
+};
+
+export default Timeline;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { ImHome } from "react-icons/im";
+
+const Navbar = () => {
+  const pathName = usePathname();
+  console.log(pathName);
+  const navLinks = (
+    <>
+      <li
+        className={`rounded-sm ${pathName === "/" ? "bg-green-900 text-white" : ""}`}
+      >
+        <Link href="/">
+          <ImHome />
+          Home
+        </Link>
+      </li>
+      <li
+        className={`rounded-sm ${pathName === "/timeline" ? "bg-green-900 text-white" : ""}`}
+      >
+        <Link href="/timeline">Timeline</Link>
+      </li>
+      <li
+        className={`rounded-sm ${pathName === "/stats" ? "bg-green-900 text-white" : ""}`}
+      >
+        <Link href="/stats">Stats</Link>
+      </li>
+    </>
+  );
+  return (
+    <div className="navbar bg-base-100 shadow-sm px-15">
+      <div className="flex-1">
+        <Link href="/" className="btn btn-ghost text-2xl">
+          KeenKeeper
+        </Link>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1 gap-4">{navLinks}</ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
